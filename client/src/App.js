32fd2f8f66bf7038c8945f2b1bf53b01acdac97a
monday-hazard <heudeclaire@ymail.com>
2021-08-31 10:40:18 +0200

    (feat) setup react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () =>
+   <div className="App">
+      <Fragment>
+         <h1>APP</h1>
+      </Fragment>
+   </div>
+
+
+
+export default App;
