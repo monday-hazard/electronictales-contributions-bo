10f7940d5e4b0be0a44bd31d44b7ceaadaa88b27
hugocrespinemiles <hugocrespinemiles@gmail.com>
2021-09-01 11:34:47 +0200

    (feat) User Model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    slackName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: String,
+        // TODO: Update role management
+        // enum: ['user', 'reviewer', 'admin'],
+        default: 'user'
+    },
+    subscriptionDate: {
+        type: Date,
+        default: Date.now
+    },
+    avatar: {
+        type: String
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
