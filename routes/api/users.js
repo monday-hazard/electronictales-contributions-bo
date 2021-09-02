2043d3ef35cf67a4f2442f91cafbcf2f569adef0
hugocrespinemiles <hugocrespinemiles@gmail.com>
2021-09-02 12:02:47 +0200

    (feat) User Authentication / Login Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,7 +30,9 @@ router.post(
 
             let user = await User.findOne({ email });
             if (user) {
-                return res.status(400).json({errors: [{ msg: 'User already exists' }]})
+                return res
+                .status(400)
+                .json({errors: [{ msg: 'User already exists' }]});
             }
 
             user = new User({
