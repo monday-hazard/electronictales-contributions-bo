9f102060e0004f8878b06a60c5e8fdab81d2f77a
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-03 11:08:35 +0200

    (chore) : delete tuto routes

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/topics', require('./routes/api/topics'));
 
 const PORT = process.env.PORT || 5000;
