df1db995649b58fa4af869e830a9e5e45acea717
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-28 15:27:22 +0200

    feat: add hero component

---
+++ client/src/components/elements/hero/Hero.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ReactComponent as DoubleChevronDown } from '../../../resources/img/icons/double-chevron-down.svg';
+import './Hero.css';
+
+const Hero = ({
+    disableSubtitle,
+    styledTitleText,
+    styledTitleClass,
+    subtitleText,
+    titleText,
+}) => {
+    return (
+        <>
+            <div className="hero-wrapper">
+                <h1 className="hero-title">
+                    {titleText}
+                    {styledTitleText && styledTitleClass && (
+                        <span className={styledTitleClass}>
+                            {styledTitleText}
+                        </span>
+                    )}
+                </h1>
+                {!disableSubtitle && (
+                    <div className="hero-subtitle">{subtitleText}</div>
+                )}
+                <DoubleChevronDown class="hero-chevron" />
+            </div>
+        </>
+    );
+};
+
+Hero.propTypes = {
+    disableSubtitle: PropTypes.bool,
+    styledTitleText: PropTypes.string,
+    styledTitleClass: PropTypes.string,
+    titleText: PropTypes.object.isRequired,
+    subtitleText: PropTypes.string,
+};
+
+export default Hero;
--- client/src/components/layout/Landing/Landing.js
+++ client/src/components/layout/Landing/Landing.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Landing.css';
+import Hero from '../../elements/hero/Hero';
 
 const Landing = () => {
-   return (
-      <div className="landing">
-         <h2>Landing</h2>
-      </div>
-   )
-}
+    const titleText = 'Bienvenue sur ';
+    const styledTitleText = 'ElectronicTales Contrib';
+    const styledTitleClass = 'site-name';
+    const subtitleText =
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, quidem accusamus totam incidunt quod a labore rerum rem nihil deleniti!';
+
+    return (
+        <div className="landing">
+            <Hero
+                titleText={titleText}
+                styledTitleText={styledTitleText}
+                styledTitleClass={styledTitleClass}
+                subtitleText={subtitleText}
+            />
+        </div>
+    );
+};
 
 export default Landing;
