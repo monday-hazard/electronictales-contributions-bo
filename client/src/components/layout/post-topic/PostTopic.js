882d75026252e6584ab54a392fdb6b50a8f6df91
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-29 12:25:06 +0200

    feat: add post topics info cards

--- client/src/components/layout/post-topic/PostTopic.js
+++ client/src/components/layout/post-topic/PostTopic.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Card from '../../elements/card/Card';
+import CardContent from '../../elements/card/CardContent';
+import Hero from '../../elements/hero/Hero';
 
 import './PostTopic.css';
 
 const PostTopic = () => {
+    const titleText = 'Proposer un sujet';
+    const subtitleText =
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, quidem accusamus totam incidunt quod a labore rerum rem nihil deleniti!';
+    const introTLTA =
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit.';
+    const introRegular =
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit.';
     return (
         <div className="post-topic">
-            <h2>Post Topic</h2>
+            <Hero titleText={titleText} subtitleText={subtitleText} />
+            <section className="topic-info">
+                <Card isSquare title="TLTA">
+                    <CardContent text={introTLTA} />
+                </Card>
+                <Card isSquare title="Regular">
+                    <CardContent text={introRegular} />
+                </Card>
+            </section>
         </div>
     );
 };
