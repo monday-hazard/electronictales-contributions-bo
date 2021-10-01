56755eb7d66739b80cc222cb8abe1006ff9a68e7
monday-hazard <heudeclaire@ymail.com>
2021-10-01 19:53:50 +0200

    create axios instance called api and refacto actions requests

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import api from '../../utils/api';
 import { setAlert } from "./alert";
 import {
    REGISTER_SUCCESS,
@@ -18,7 +18,7 @@ export const loadUser = () => async dispatch => {
    }
 
    try {
-      const res = await axios.get('/api/auth');
+      const res = await api.get('/auth');
 
       dispatch({
          type: USER_LOADED,
@@ -41,7 +41,7 @@ export const register = ({ userName, slackName, email, password }) => async disp
    }
    const body = JSON.stringify({ userName, slackName, email, password });
    try {
-      const res = await axios.post('/api/users', body, config);
+      const res = await api.post('/users', body, config);
       dispatch({
          type: REGISTER_SUCCESS,
          payload: res.data
@@ -68,7 +68,7 @@ export const login = (email, password) => async dispatch => {
    }
    const body = JSON.stringify({ email, password });
    try {
-      const res = await axios.post('/api/auth', body, config);
+      const res = await api.post('/auth', body, config);
 
       dispatch({
          type: LOGIN_SUCCESS,
