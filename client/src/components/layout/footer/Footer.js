e0f2256b261fdbc116ec4f5c74cb558686d43742
monday-hazard <heudeclaire@ymail.com>
2021-10-04 15:30:32 +0200

    design footer & icons

--- client/src/components/layout/footer/Footer.js
+++ client/src/components/layout/footer/Footer.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import facebookIcon from '../../../resources/img/icons/social-icons/facebook.svg';
+import twitterIcon from '../../../resources/img/icons/social-icons/twitter.svg';
+import githubIcon from '../../../resources/img/icons/social-icons/github.svg';
+import linkedinIcon from '../../../resources/img/icons/social-icons/linkedin.svg';
+import instagramIcon from '../../../resources/img/icons/social-icons/instagram.svg';
+import slackIcon from '../../../resources/img/icons/social-icons/slack.svg';
+import emailIcon from '../../../resources/img/icons/social-icons/email.svg';
 
 import "./Footer.css";
 
@@ -12,24 +19,27 @@ const Footer = (props) => {
       && <footer>
 
          <p>Made with &#10084; by Electronic&nbsp;Tales Team</p>
-         <div>
+         <div className="social-list">
             <a href="mailto:contact@electronictales.io">
-               <i className="fas fa-envelope"></i>
+               <img className="social-icon-footer" src={emailIcon} alt="Email" />
             </a>
             <a href="https://www.instagram.com/electronictales.io/">
-               <i className="fab fa-instagram"></i>
+               <img className="social-icon-footer" src={instagramIcon} alt="Instagram" />
             </a>
             <a href="https://twitter.com/ElectronicTales">
-               <i className="fab fa-twitter"></i>
+               <img className="social-icon-footer" src={twitterIcon} alt="Twitter" />
             </a>
             <a href="https://www.facebook.com/Electronic-Tales-100499265012350">
-               <i className="fab fa-facebook"></i>
+               <img className="social-icon-footer" src={facebookIcon} alt="Facebook" />
             </a>
             <a href="https://www.linkedin.com/company/34911130">
-               <i className="fab fa-linkedin"></i>
+               <img className="social-icon-footer" src={linkedinIcon} alt="LinkedIn" />
             </a>
             <a href="https://github.com/OfficierAzarov/electronic-tales-presents">
-               <i className="fab fa-github"></i>
+               <img className="social-icon-footer" src={githubIcon} alt="Github" />
+            </a>
+            <a href="http://platform.electronictales.io/safe-space/chat">
+               <img className="social-icon-footer" src={slackIcon} alt="Notre slack" />
             </a>
          </div>
       </footer>
