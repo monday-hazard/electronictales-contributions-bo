aad1f18022e0126ced8e1bbf3f418ab0ff694f0d
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-10-05 12:16:35 +0200

    feat: register form validation schema

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -12,7 +12,10 @@ import TitleSection from '../elements/page-section/title-section/TitleSection';
 import { REGISTER_SUCCESS_MODAL_CONTENT } from '../../dictionnary/modalContentList';
 import { REGISTER_ERROR_PASSWORD_ALERT_CONTENT } from '../../dictionnary/alertContentList';
 
+import { passwordCheckingRegex, emailCheckingRegex } from '../../utils/regex';
+
 import './Form.css';
+import './Register.css';
 
 const Register = ({ setAlert, register }) => {
    const [formData, setFormData] = useState({
@@ -22,15 +25,47 @@ const Register = ({ setAlert, register }) => {
       password: '',
       confirm_password: '',
    });
-
    const { userName, email, slackname, password, confirm_password } = formData;
+   const [userNameNotValid, setUserNameNotValid] = useState(undefined);
+   const [emailNotValid, setEmailNotValid] = useState(undefined);
+   const [passwordNotStrong, setPasswordNotStrong] = useState(undefined);
+   const [unmatchingPasswords, setUnmatchingPasswords] = useState(undefined);
 
-   const onChange = (e) =>
+   const onChange = (e) => {
+      switch (e.target.name) {
+         case 'userName':
+            e.target.value = e.target.value.trim();
+            e.target.value.length >= 2
+               ? setUserNameNotValid(false)
+               : setUserNameNotValid(true);
+            break;
+         case 'email':
+            e.target.value = e.target.value.trim();
+            emailCheckingRegex.test(e.target.value)
+               ? setEmailNotValid(false)
+               : setEmailNotValid(true);
+            break;
+         case 'password':
+            passwordCheckingRegex.test(e.target.value)
+               ? setPasswordNotStrong(false)
+               : setPasswordNotStrong(true);
+            break;
+         case 'confirm_password':
+            password !== e.target.value
+               ? setUnmatchingPasswords(true)
+               : setUnmatchingPasswords(false);
+         case 'slackname':
+            e.target.value = e.target.value.trim();
+         default:
+            break;
+         }
+
       setFormData({ ...formData, [e.target.name]: e.target.value });
+   }
 
    const onSubmit = e => {
       e.preventDefault();
-      if (password !== confirm_password) {
+      if (unmatchingPasswords) {
          setAlert(
             REGISTER_ERROR_PASSWORD_ALERT_CONTENT.message,
             'error'
@@ -47,26 +82,38 @@ const Register = ({ setAlert, register }) => {
                <TitleSection title="S'inscrire" subtitle="Inscris-toi et crée ton espace personnel" />
                <form className='form' onSubmit={e => onSubmit(e)} >
                   <Input
+                     required
                      label={{ className: "picto account-picto", position: "before" }}
-                     className='input-form'
+                     className={'input-form'
+                        + `${userNameNotValid === undefined ? ''
+                           : userNameNotValid ? ' invalid-input' : ' valid-input'}`}
                      type='text'
                      name='userName'
                      placeholder="Ton pseudo *"
                      value={userName}
                      onChange={(e) => onChange(e)}
-                     required
+                     minLength={2}
                   />
+                  {userNameNotValid && (
+                     <span className="error-label">Le username n'est pas valide</span>
+                  )}
                   <Input
+                     required
                      label={{ className: "picto email-picto", position: "before" }}
-                     className='input-form'
+                     className={'input-form'
+                        + `${emailNotValid === undefined ? ''
+                           : emailNotValid ? ' invalid-input' : ' valid-input'}`}
                      type='email'
                      name='email'
                      placeholder='Ton email *'
                      value={email}
                      onChange={(e) => onChange(e)}
-                     required
                   />
+                  {emailNotValid && (
+                     <span className="error-label">L'email n'est pas valide</span>
+                  )}
                   <Input
+                     required
                      label={{ className: "picto slack-picto", position: "before" }}
                      className='input-form'
                      type='text'
@@ -77,24 +124,37 @@ const Register = ({ setAlert, register }) => {
                   />
                   <Input
                      label={{ className: "picto password-picto", position: "before" }}
-                     className='input-form'
+                     className={'input-form'
+                        + `${passwordNotStrong === undefined ? ''
+                           : passwordNotStrong ? ' invalid-input' : ' valid-input'}`
+                     }
                      type='password'
                      name='password'
                      placeholder='Un mot de passe *'
                      value={password}
                      onChange={(e) => onChange(e)}
-                     required
+                     minLength={6}
                   />
+                  {passwordNotStrong && (
+                     <span className="error-label">Le mot de passe n'est pas assez fort</span>
+                  )}
                   <Input
+                     required
                      label={{ className: "picto validate-picto", position: "before" }}
-                     className='input-form'
+                     className={'input-form'
+                        + `${unmatchingPasswords === undefined ? ''
+                           : unmatchingPasswords ? ' invalid-input' : ' valid-input'}`
+                     }
                      type='password'
                      name='confirm_password'
                      placeholder='Confirme ton mot de passe *'
                      value={confirm_password}
                      onChange={(e) => onChange(e)}
-                     required
+                     minLength={6}
                   />
+                  {unmatchingPasswords && (
+                     <span className="error-label">Les mots de passe ne correspondent pas</span>
+                  )}
                   <Input
                      type='submit'
                      value="Zou, je m'inscris !"
